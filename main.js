1400f45a8a9619933d1a9d642c64fc32750afaae
richieeinstein1 <37557835+richieeinstein1@users.noreply.github.com>
2020-11-25 20:14:25 +0000

    Update main.js

--- main.js
+++ main.js
@@ -10,10 +10,10 @@ var days = Math.floor(t / (1000 * 60 * 60 * 24));
 var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60)); 
 var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60)); 
 var seconds = Math.floor((t % (1000 * 60)) / 1000); 
-document.getElementById("day").innerHTML =days + " d" ; 
-document.getElementById("hour").innerHTML =hours + " h"; 
-document.getElementById("minute").innerHTML = minutes + " m"; 
-document.getElementById("second").innerHTML =seconds + " s"; 
+document.getElementById("day").innerHTML =days + "d" ; 
+document.getElementById("hour").innerHTML =hours + "h"; 
+document.getElementById("minute").innerHTML = minutes + "m"; 
+document.getElementById("second").innerHTML =seconds + "s"; 
 
 }
 
